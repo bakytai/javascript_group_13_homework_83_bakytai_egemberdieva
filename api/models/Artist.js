b304f7bf441d653594688182d947943300a30f80
Bakytai Egemberdieva <bakijw@gmail.com>
2022-02-24 23:25:00 +0600

    создала сущность для Артиста

---
+++ api/models/Artist.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    artist: {
+        type: Schema.Types.ObjectId,
+        ref: 'Artist',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+
+    year: {
+        type: Number,
+        required: true
+    },
+    image: String
+});
+
+const Album = mongoose.model('Album', AlbumSchema);
+
+module.exports = Album;
