53fa4b30cd6daa2970fcafd86740abdd17e1038d
Bakytai Egemberdieva <bakijw@gmail.com>
2022-02-25 19:52:55 +0600

    Прописала эффекты на запросы для альбомов

---
+++ frontend/src/app/store/album.effects.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { AlbumsService } from '../services/albums.service';
+import { catchError, mergeMap, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import {
+  createAlbumFailure,
+  createAlbumRequest,
+  createAlbumSuccess,
+  fetchAlbumsFailure,
+  fetchAlbumsRequest,
+  fetchAlbumsSuccess
+} from './album.actions';
+
+@Injectable()
+
+export class AlbumEffects {
+  fetchProducts = createEffect(() => this.actions.pipe(
+    ofType(fetchAlbumsRequest),
+    mergeMap((artistId:{id: string}) => this.albumService.getAlbums(artistId.id).pipe(
+      map(albums => fetchAlbumsSuccess({albums})),
+      catchError(() => of(fetchAlbumsFailure({error: 'Something went wrong'})))
+    ))
+  ));
+
+  createAlbum= createEffect(() => this.actions.pipe(
+    ofType(createAlbumRequest),
+    mergeMap(({albumData}) => this.albumService.createAlbum(albumData).pipe(
+      map(() => createAlbumSuccess()),
+      tap(() => this.router.navigate(['/'])),
+      catchError(() => of(createAlbumFailure({error: 'Wrong Data'})))
+    ))
+  ));
+
+
+  constructor(
+    private router: Router,
+    private actions: Actions,
+    private albumService: AlbumsService
+  ) {}
+}
