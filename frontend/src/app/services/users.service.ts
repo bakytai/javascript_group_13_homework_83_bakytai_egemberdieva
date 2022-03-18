c3df9fc1e40e367bfbe88d3f2f8fa756d454b332
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-18 17:21:06 +0600

    добавила запросы в сервис юзера, и создала сервис для отображения сообщений на странице

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { RegisterUserData, User } from '../models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginUserData, RegisterUserData, User } from '../models/user.model';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -20,5 +20,15 @@ export class UsersService {
     });
 
     return this.http.post<User>(environment.apiUrl + '/users', userData);
+  }
+
+  login(userData: LoginUserData) {
+    return this.http.post<User>(environment.apiUrl + '/users/sessions', userData);
+  }
+
+  logout(token: string) {
+    return this.http.delete(environment.apiUrl + '/users/sessions', {
+      headers: new HttpHeaders({'Authorization': token})
+    });
   }
 }
