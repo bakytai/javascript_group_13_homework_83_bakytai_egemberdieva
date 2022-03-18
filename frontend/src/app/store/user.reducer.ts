a0709cb56b757eeb40705da73c85a661c0f7d38a
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-18 17:22:49 +0600

    дописала reducer и effects для Юзера

--- frontend/src/app/store/user.reducer.ts
+++ frontend/src/app/store/user.reducer.ts
@@ -1,16 +1,29 @@
 import { UserState } from './types';
 import { createReducer, on } from '@ngrx/store';
-import { registerUserFailure, registerUserRequest, registerUserSuccess } from './user.actions';
+import {
+  loginUserFailure,
+  loginUserRequest,
+  loginUserSuccess, logoutUser,
+  registerUserFailure,
+  registerUserRequest,
+  registerUserSuccess
+} from './user.actions';
 
 const initialState: UserState = {
   user: null,
   registerLoading: false,
-  registerError: null
+  registerError: null,
+  loginLoading: false,
+  loginError: null
 };
 
 export const userReducer = createReducer(
   initialState,
   on(registerUserRequest, state => ({...state, registerLoading: true, registerError: null})),
   on(registerUserSuccess, (state, {user}) => ({...state, registerLoading: false, user})),
-  on(registerUserFailure, (state, {error}) => ({...state, registerLoading: false, registerError: error}))
+  on(registerUserFailure, (state, {error}) => ({...state, registerLoading: false, registerError: error})),
+  on(loginUserRequest, state => ({...state, loginLoading: true, loginError: null,})),
+  on(loginUserSuccess, (state, {user}) => ({...state, loginLoading: false, user})),
+  on(loginUserFailure, (state, {error}) => ({...state, loginLoading: false, loginError: error})),
+  on(logoutUser, state => ({...state, user: null,}))
 );
