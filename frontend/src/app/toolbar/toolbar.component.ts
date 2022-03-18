99f87ef10ff9b7800a67cd6c8661c62e1d0fbcc3
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-18 17:24:01 +0600

    Скрываю меню пользователя от незарегестрированного пользователя

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/types';
+import { logoutUserRequest } from '../store/user.actions';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.sass']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent  {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
+  user: Observable<null | User>;
 
-  ngOnInit(): void {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private store: Store<AppState>
+  ) {
+    this.user = store.select(state => state.users.user);
+  }
+
+  logout() {
+    this.store.dispatch(logoutUserRequest());
   }
 
 }
