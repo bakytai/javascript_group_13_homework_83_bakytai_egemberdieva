efde87ec7375df003e1b796bb64beee5c4abdbf9
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-22 12:17:21 +0600

    Добавила форму для создания альбомов

--- frontend/src/app/artists-albums/edit-albums/edit-albums.component.ts
+++ frontend/src/app/artists-albums/edit-albums/edit-albums.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/types';
+import { Artist } from '../../models/artist.model';
+import { fetchArtistRequest } from '../../store/artist.actions';
 
 @Component({
   selector: 'app-edit-albums',
@@ -11,15 +13,21 @@ import { AppState } from '../../store/types';
 })
 export class EditAlbumsComponent implements OnInit {
   @ViewChild('f') form!: NgForm;
+  artists: Observable<Artist[]>;
   error: Observable<string | null>;
   loading: Observable<boolean>;
 
   constructor(private store: Store<AppState>) {
     this.loading = store.select(state => state.albums.createLoading);
     this.error = store.select(state => state.albums.createError);
+    this.artists = store.select(state => state.artists.artists);
   }
 
   ngOnInit(): void {
+    this.store.dispatch(fetchArtistRequest());
   }
 
+  onSubmit() {
+
+  }
 }
