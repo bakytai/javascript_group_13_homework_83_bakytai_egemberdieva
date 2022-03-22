c44190fb9d4f4be3ea94cfe22085f9432b9b5f27
Bakytai Egemberdieva <bakijw@gmail.com>
2022-03-22 12:31:13 +0600

    Добавила форму для создания треков

--- frontend/src/app/tracks/edit-tracks/edit-tracks.component.ts
+++ frontend/src/app/tracks/edit-tracks/edit-tracks.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/types';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Album } from '../../models/album.model';
 
 @Component({
   selector: 'app-edit-tracks',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-tracks.component.sass']
 })
 export class EditTracksComponent implements OnInit {
+  @ViewChild('f') form!: NgForm;
+  albums: Observable<Album[]>;
+  error: Observable<string | null>;
+  loading: Observable<boolean>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {
+    this.loading = store.select(state => state.albums.createLoading);
+    this.error = store.select(state => state.albums.createError);
+    this.albums = store.select(state => state.albums.albums);
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+
+  }
 }
